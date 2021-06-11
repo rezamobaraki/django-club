96ee76d91b447a17642d280d30cd7336c926d5d8
MrRezoo <rezam578@gmail.com>
2021-06-11 22:30:53 +0430

    github oath

--- DjangoClub/urls.py
+++ DjangoClub/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('o/', include('social_django.urls', namespace='social'))
+    path('oauth/', include('social_django.urls', namespace='social'))
 ]
