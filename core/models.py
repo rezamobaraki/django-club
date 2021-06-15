5e9fad8cca4169285ecaa77b1e1f00fa8ea40ba2
Mr.Rezoo <rezam578@gmail.com>
2021-06-15 19:37:57 +0430

    refactor Question Model

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from django.utils.text import slugify
+
+
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
     title = models.CharField(max_length=200)
@@ -13,6 +15,10 @@ class Question(models.Model):
 
     def __str__(self):
         return f"{self.user} - {self.title[:20]}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class Answer(models.Model):
